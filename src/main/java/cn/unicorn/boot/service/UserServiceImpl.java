add72b567783dbb29b4ea43fcb329af87b6a531f
Naidong Xu <Rose.Dawson.Ocean@gmail.com>
2014-05-08 16:16:57 +0800

    修改构建文件

--- src/main/java/cn/unicorn/boot/service/UserServiceImpl.java
+++ src/main/java/cn/unicorn/boot/service/UserServiceImpl.java
@@ -1,22 +1,22 @@
 package cn.unicorn.boot.service;
 
-import javax.transaction.Transactional;
-import javax.transaction.Transactional.TxType;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import cn.unicorn.boot.dao.UserRepository;
 import cn.unicorn.boot.domain.User;
 
-@Service("userService")
+@Service
 public class UserServiceImpl implements IUserService {
 	@Autowired
 	private UserRepository userRepository;
 	//private static Logger _log = LoggerFactory.getLogger(UserServiceImpl.class);
 
-	@Transactional(value = TxType.NOT_SUPPORTED)
+	@Transactional(readOnly=true)
 	public boolean checkLogin(String username, String password) {
 		Assert.hasText(username, "username is blank!");
 		Assert.hasText(password, "password is blank!");
@@ -24,30 +24,30 @@ public class UserServiceImpl implements IUserService {
 		return user != null && user.getPassword().equals(password);
 	}
 
-	@Transactional(value = TxType.NOT_SUPPORTED)
+	@Transactional(readOnly=true)
 	public User getUser(String userNo) {
 		Assert.hasText(userNo, "userNo is not blank!");
 		return userRepository.findOne(userNo);
 	}
 
-	@Transactional(value = TxType.REQUIRED, rollbackOn = Exception.class)
+	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
 	public void saveOrUpdateUser(User user) {
 		Assert.notNull(user, "user is null!");
 		userRepository.save(user);
 	}
 
-	@Transactional(value = TxType.REQUIRED, rollbackOn = Exception.class)
+	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
 	public void deleteUser(User user) {
 		Assert.notNull(user, "user is null!");
 		userRepository.delete(user);
 	}
 
-	@Transactional(value = TxType.REQUIRED, rollbackOn = Exception.class)
+	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
 	public void deleteUserByNo(String userNo) {
 		Assert.hasText(userNo, "userNo is null!");
 		userRepository.delete(userNo);
 	}
-	@Transactional(value = TxType.NOT_SUPPORTED)
+	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
 	public Iterable<User> findAllUser() {
 		return userRepository.findAll();
 	}
